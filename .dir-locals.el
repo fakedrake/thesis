1148ace65b99a0dc8fb6f3d7129d21e667a829d2
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2016-02-28 12:52:45 +0200

    Added some variables

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (org-latex-packages-alist . (("" "listings")))
+  (org-latex-listings . t)
+  (org-latex-listings-options . (("columns" "fixed")
+                                 ("breaklines")
+                                 ("basicstyle" "\\ttfamily\\footnotesize")
+                                 ("keywordstyle" "\\bfseries")))))
