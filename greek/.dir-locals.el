2eaff9e8eae8ba827711595a6d63a342e4204587
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2016-05-16 01:29:55 +0300

    Translation at 25%

---
+++ greek/.dir-locals.el
@@ -0,0 +1,18 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (google-translate-default-source-language . "en")
+  (google-translate-default-target-language . "el")
+  (org-latex-packages-alist . (("" "listings") ("" "float")))
+  (org-latex-listings . t)
+  (ispell-dictionary . "el")
+  (org-latex-listings-options . (("columns" "fixed")
+                                 ("breaklines")
+                                 ("basicstyle" "\\ttfamily\\small")
+                                 ("keywordstyle" "\\bfseries")
+                                 ("frame" "tlbr")
+                                 ("xleftmargin" "10pt")
+                                 ("xrightmargin" "10pt")
+                                 ("framesep" "10pt")
+                                 ("framerule" "0pt")))))
